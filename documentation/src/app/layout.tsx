2fb8c8ea8055972a6a708d946b0d89a9690a4502
zion <zion@nyu.edu>
2026-02-15 02:35:12 +0600

    change giggles sidebar title font

--- documentation/src/app/layout.tsx
+++ documentation/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist_Mono } from 'next/font/google';
+import { Geist_Mono, Barriecito } from 'next/font/google';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 
@@ -6,9 +6,15 @@ const geistMono = Geist_Mono({
   subsets: ['latin']
 });
 
+const barriecito = Barriecito({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-barriecito',
+});
+
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={geistMono.className} suppressHydrationWarning>
+    <html lang="en" className={`${geistMono.className} ${barriecito.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
