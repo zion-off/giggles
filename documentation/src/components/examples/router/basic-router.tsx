14e7ecfc46fed8ed03680b8a03c1ac6dca4f9d47
zion <zion@nyu.edu>
2026-02-16 02:28:50 +0600

    docs: create router documentation

---
+++ documentation/src/components/examples/router/basic-router.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { GigglesProvider, Router, Screen, useFocus, useKeybindings, useNavigation } from 'giggles';
+import { Box, Text } from 'ink-web';
+
+function Home() {
+  const { push } = useNavigation();
+  const focus = useFocus();
+
+  useKeybindings(focus, {
+    s: () => push('settings'),
+    d: () => push('detail', { id: 42 })
+  });
+
+  return (
+    <Box flexDirection="column">
+      <Text bold>Home</Text>
+      <Text dimColor>s = settings, d = detail</Text>
+    </Box>
+  );
+}
+
+function Settings() {
+  const { pop } = useNavigation();
+  const focus = useFocus();
+
+  useKeybindings(focus, {
+    q: () => pop()
+  });
+
+  return (
+    <Box flexDirection="column">
+      <Text bold>Settings</Text>
+      <Text dimColor>q = back</Text>
+    </Box>
+  );
+}
+
+function Detail() {
+  const { pop, currentRoute } = useNavigation();
+  const focus = useFocus();
+
+  useKeybindings(focus, {
+    q: () => pop()
+  });
+
+  return (
+    <Box flexDirection="column">
+      <Text bold>Detail (id: {String(currentRoute.params?.id)})</Text>
+      <Text dimColor>q = back</Text>
+    </Box>
+  );
+}
+
+export default function BasicRouterExample() {
+  return (
+    <GigglesProvider>
+      <Router initialScreen="home">
+        <Screen name="home" component={Home} />
+        <Screen name="settings" component={Settings} />
+        <Screen name="detail" component={Detail} />
+      </Router>
+    </GigglesProvider>
+  );
+}
