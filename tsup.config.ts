e9f5bb201a9af51466fd6ad8c1812d60aeb02c85
zion <zion@nyu.edu>
2026-02-19 05:18:34 +0600

    specify terminal as an entry point in package.json and tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/terminal/index.ts'],
   format: ['esm'],
   target: 'node16',
   clean: true,
