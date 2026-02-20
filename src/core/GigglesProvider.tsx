ceb2772ba46ef5474235a67b5082a7fd1b54dd39
zion <zion@nyu.edu>
2026-02-21 01:10:35 +0600

    feat: add theme system with ThemeProvider and useTheme
    Configurable colors and indicators via GigglesProvider theme prop.
    Nested themes supported via ThemeProvider.

--- src/core/GigglesProvider.tsx
+++ src/core/GigglesProvider.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { AlternateScreen } from '../terminal/components/AlternateScreen';
 import { FocusProvider } from './focus';
 import { InputProvider, InputRouter } from './input';
+import { type GigglesTheme, ThemeProvider } from './theme';
 
 type GigglesProviderProps = {
+  theme?: Partial<GigglesTheme>;
   children: React.ReactNode;
 };
 
-export function GigglesProvider({ children }: GigglesProviderProps) {
+export function GigglesProvider({ theme, children }: GigglesProviderProps) {
   return (
     <AlternateScreen>
-      <FocusProvider>
-        <InputProvider>
-          <InputRouter>{children}</InputRouter>
-        </InputProvider>
-      </FocusProvider>
+      <ThemeProvider theme={theme}>
+        <FocusProvider>
+          <InputProvider>
+            <InputRouter>{children}</InputRouter>
+          </InputProvider>
+        </FocusProvider>
+      </ThemeProvider>
     </AlternateScreen>
   );
 }
