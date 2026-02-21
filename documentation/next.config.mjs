94136fecac0d499a1f13515f653bcaa02c161967
zion <zion@nyu.edu>
2026-02-21 17:23:35 +0600

    add --force flag to vercel deploy

--- documentation/next.config.mjs
+++ documentation/next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  outputFileTracingExcludes: {
+    '*': ['../node_modules/**', '../src/**', '../playground/**', '../tools/**']
+  },
   // Alias 'ink' to 'ink-web' on the client so giggles (and other
   // packages that import from 'ink') use the browser-compatible build.
   serverExternalPackages: ['ink'],
