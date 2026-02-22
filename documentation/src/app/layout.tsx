c88876e14e1e5aaf14979eadcb520f855da5ab46
zion <zion@nyu.edu>
2026-02-22 19:05:09 +0600

    feat(docs): migrate to static/client-side search

--- documentation/src/app/layout.tsx
+++ documentation/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Barriecito, Geist_Mono } from 'next/font/google';
-import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
+import { Provider } from './provider';
 
 const geistMono = Geist_Mono({
   subsets: ['latin']
@@ -16,7 +16,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={`${geistMono.className} ${barriecito.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
